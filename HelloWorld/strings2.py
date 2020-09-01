c72b83600e0543869c2d3743511d6608f3fd6350
Jenni Heikkilä <jenni.heikkila89@gmail.com>
2020-09-01 16:08:04 +0300

    More testing
    Playing with strings

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -2,7 +2,12 @@
 #         012345678901234
 parrot = "Norwegian Blue"
 
-print(parrot[0:6])  # Norweg
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
+print(parrot[0:6:2])    # Nre
+print(parrot[0:6:3])    # Nw
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
