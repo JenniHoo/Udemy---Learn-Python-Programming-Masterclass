7178dfdcb01238e7bc1f9c1d34ba10786d6fdf82
Jenni Heikkilä <jenni.heikkila89@gmail.com>
2020-09-07 15:01:04 +0300

    Fix
    Small fix to get rid of some extra text

--- HelloWorld/interpolation.py
+++ HelloWorld/interpolation.py
@@ -6,7 +6,3 @@ minor = "months"
 print("My age is %d %s, %d %s" % (age, major, 6, minor))
 print("Pi is approximately %f" % (22 / 7))
 print("Pi is approximately %60.50f" % (22 / 7))
-
-print()
-flower = "blue violet"
-print('blue' in flower)
